64903d2bc9eea8c480da21d18c7b6d889f83a00f
Giovana040 <souza.barbosa.giovana@escola.pr.gov.br>
2025-09-12 12:03:12 -0300

    update

--- dom_giovana.js
+++ dom_giovana.js
@@ -1,1 +1,9 @@
-            
+const rodri_circulo = document.jetElementById('circulo')
+ let tamanho_rodri = 10
+
+ rodri_circulo.addEventListener('dblclik'),()=>{
+    rodri_circulo.style.borderadius = `${tamanho_rodri}px`
+    rodri_circulo.style.width = `${tamanho_rodri}px`
+    rodri_circul.style.helght = `${tamanho_rodri}px`
+    tamanho_rodri = tamanho_rodri+20,
+ }
